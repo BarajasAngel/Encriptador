44f55e92a0768e2fc365afe5d6f553a3aade7277
Angel Barajas <Barajasb15@gmail.com>
2022-08-29 19:19:49 -0500

    add: script para copiado

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
+var clipboard = new Clipboard('.btn');
 
+clipboard.on('success', function(e) {
+console.info('Accion:', );
+swal("Copiado", "Ahora colócalo donde puedas desencriptarlo!", "success");
 
-function limpiarInput1(){
-    var input1 = document.getElementById("input1").value;
-    if(input1 == "Ingresa el texto aquí"){
-        document.getElementById("input1").value = "";
-    }
-}
-
+e.clearSelection();
+});
 
 function Encriptar(){
     var encriptado = "";
     var input1 = document.getElementById("input1").value;
 
     if(!/[A-Z\u003A-\u0060\u007B-\u10FFFF\x23-\x2D]/.test(input1)){
-        encriptado = input1.replace(/a/g,"ai").replace(/e/g,"enter").replace(/i/g,"imes").replace(/o/g,"ober").replace(/u/g,"ufat");
-        alert(encriptado);
-        
+        encriptado = input1.replace(/e/g,"enter").replace(/i/g,"imes").replace(/a/g,"ai").replace(/o/g,"ober").replace(/u/g,"ufat");        
+        document.getElementById("input2").value = encriptado;        
         document.getElementById("advertencia").style.color = "#495057";
+        document.getElementById("imagen").style.display = "none";            
+        document.getElementById("desencriptar").className = "desencriptar-mostrar"    ;
     }
     else{
         document.getElementById("advertencia").style.color = "red";        
@@ -27,10 +27,11 @@ function Desencriptar(){
     var desencriptado = "";
     var input1 = document.getElementById("input1").value;
     if(!/[A-Z\u003A-\u0060\u007B-\u10FFFF\x23-\x2D]/.test(input1)){
-        desencriptado = input1.replace(/ai/g,"a").replace(/enter/g,"e").replace(/imes/g,"i").replace(/ober/g,"o").replace(/ufat/g,"u");                
-        document.getElementById("advertencia").style.color = "#495057";
-        alert(desencriptado);
+        desencriptado = input1.replace(/enter/g,"e").replace(/imes/g,"i").replace(/ai/g,"a").replace(/ober/g,"o").replace(/ufat/g,"u");   
+        document.getElementById("input2").value = desencriptado;             
+        document.getElementById("advertencia").style.color = "#495057";        
     }else{
         document.getElementById("advertencia").style.color = "red";        
     }
 }
+
